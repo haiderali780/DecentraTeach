e211b6984c92231a2a1a5e4288304d66c2516cad
HassanShahzad36 <hassanshahzad4563@gmail.com>
2025-03-02 14:24:53 +0500

    instructor-controller

---
+++ server/controllers/instructor-controller/course-controller.js
@@ -0,0 +1,74 @@
+const Course = require('../../models/Course');
+
+const addNewCourse = async (req, res) => {
+  try {
+    const courseData = req.body;
+    const newlyCreatedCourse = new Course(courseData);
+    const saveCourse = await newlyCreatedCourse.save();
+
+    if (saveCourse) {
+      res.status(201).json({
+        success: true,
+        message: 'Course saved successfully',
+        data: saveCourse,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: 'Some error occured!',
+    });
+  }
+};
+
+const getAllCourses = async (req, res) => {
+  console.log(req);
+  try {
+    const coursesList = await Course.find({});
+
+    res.status(200).json({
+      success: true,
+      data: coursesList,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: 'Some error occured!',
+    });
+  }
+};
+
+const getCourseDetailsByID = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const courseDetails = await Course.findById(id);
+
+    if (!courseDetails) {
+      return res.status(404).json({
+        success: false,
+        message: 'Course not found!',
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: courseDetails,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: 'Some error occured!',
+    });
+  }
+};
+
+
+
+module.exports = {
+  addNewCourse,
+  getAllCourses,
+  getCourseDetailsByID,
+};
